48b0df8230142a31ffc6eab29f971599532fa95f
iamkupoe <samuelkupoe1@gmail.com>
2021-12-27 16:52:02 -0600

    product card created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import chair from "./images/long.jpg";
 
-function App() {
+function productCard() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <div className="product-card">
+    <span className="card-tag">#1 Best Seller</span>
+    <div className="card-header">
+      <img src={chair} alt="chair"/>
     </div>
+    <div className="card-body">
+      <h4 className="product-title" >Easy Relax Chair</h4>
+      <p className="product-status">Ships in 3-4 days</p>
+      <h5 className="product-price">$100.00</h5>
+    </div>
+    <div className="card-footer">
+      <button className="btn btn-secondary">Save</button>
+      <button className="btn btn-primary">Buy</button>
+    </div>
+  </div>
+  </div>
   );
 }
 
-export default App;
+export default productCard;
